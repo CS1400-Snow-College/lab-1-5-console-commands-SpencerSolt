b532521fd02ab38306d85bf559ac061f1ad243eb
SpencerSolt <spencer.solt@students.snow.edu>
2025-09-17 09:37:36 -0600

    Complete project

--- Program.cs
+++ Program.cs
@@ -1,2 +1,209 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+//Cyan
+Console.BackgroundColor = ConsoleColor.Cyan; //Changes the text and background color to the set color, the same for similar lines later
+Console.ForegroundColor = ConsoleColor.Cyan;
+Console.SetCursorPosition(0, 0); //Row 0, Changes the position of the cursor so it can tyoe in the correct location, same for similar lines
+Console.Write("              "); //Types the colored squared represented by spaces
+Console.SetCursorPosition(0, 1); //Row 1
+Console.Write("    ");
+Console.SetCursorPosition(9, 1);
+Console.Write("     ");
+Console.SetCursorPosition(0, 2); //Row 2
+Console.Write("   ");
+Console.SetCursorPosition(12, 2);
+Console.Write("  ");
+Console.SetCursorPosition(0, 3); //Row 3
+Console.Write("   ");
+Console.SetCursorPosition(10, 3);
+Console.Write("    ");
+Console.SetCursorPosition(0, 4); //Row 4
+Console.Write("  ");
+Console.SetCursorPosition(12, 4);
+Console.Write("  ");
+Console.SetCursorPosition(0, 5); //Row 5
+Console.Write("  ");
+Console.SetCursorPosition(13, 5);
+Console.Write(" ");
+Console.SetCursorPosition(0, 6); //Row 6
+Console.Write("  ");
+Console.SetCursorPosition(12, 6);
+Console.Write("  ");
+Console.SetCursorPosition(0, 7); //Row 7
+Console.Write("    ");
+Console.SetCursorPosition(11, 7);
+Console.Write("   ");
+Console.SetCursorPosition(0, 8); //Row 8
+Console.Write("   ");
+Console.SetCursorPosition(9, 8);
+Console.Write("     ");
+Console.SetCursorPosition(0, 9); //Row 9
+Console.Write("  ");
+Console.SetCursorPosition(12, 9);
+Console.Write("  ");
+Console.SetCursorPosition(0, 10); //Row 10
+Console.Write(" ");
+Console.SetCursorPosition(13, 10);
+Console.Write(" ");
+Console.SetCursorPosition(0, 11); //Row 11
+Console.Write(" ");
+Console.SetCursorPosition(13, 11);
+Console.Write(" ");
+Console.SetCursorPosition(0, 12); //Row 12
+Console.Write(" ");
+Console.SetCursorPosition(13, 12);
+Console.Write(" ");
+Console.SetCursorPosition(0, 13); //Row 13
+Console.Write(" ");
+Console.SetCursorPosition(13, 13);
+Console.Write(" ");
+Console.SetCursorPosition(0, 14); //Row 14
+Console.Write("   ");
+Console.SetCursorPosition(6, 14);
+Console.Write("  ");
+Console.SetCursorPosition(11, 14);
+Console.Write("   ");
+Console.SetCursorPosition(0, 15); //Row 15
+Console.Write("  ");
+Console.SetCursorPosition(5, 15);
+Console.Write("   ");
+Console.SetCursorPosition(11, 15);
+Console.Write("   ");
+Console.SetCursorPosition(0, 16); //Row 16
+Console.Write(" ");
+Console.SetCursorPosition(5, 16);
+Console.Write("   ");
+Console.SetCursorPosition(12, 16);
+Console.Write("  ");
+Console.SetCursorPosition(0, 17); //Row 17
+Console.Write("              ");
+//Red
+Console.BackgroundColor = ConsoleColor.Red;
+Console.ForegroundColor = ConsoleColor.Red;
+Console.SetCursorPosition(4, 1); //Row 1
+Console.Write("     ");
+Console.SetCursorPosition(3, 2); //Row 2
+Console.Write("         ");
+Console.SetCursorPosition(5, 8); //Row 8
+Console.Write(" ");
+Console.SetCursorPosition(5, 9); //Row 9
+Console.Write(" ");
+Console.SetCursorPosition(8, 9);
+Console.Write(" ");
+Console.SetCursorPosition(5, 10); //Row 10
+Console.Write(" ");
+Console.SetCursorPosition(8, 10);
+Console.Write(" ");
+Console.SetCursorPosition(5, 11); //Row 11
+Console.Write("    ");
+Console.SetCursorPosition(4, 12); //Row 12
+Console.Write(" ");
+Console.SetCursorPosition(6, 12);
+Console.Write("  ");
+Console.SetCursorPosition(9, 12);
+Console.Write(" ");
+Console.SetCursorPosition(3, 13); //Row 13
+Console.Write("        ");
+Console.SetCursorPosition(3, 14); //Row 14
+Console.Write("   ");
+Console.SetCursorPosition(8, 14);
+Console.Write("   ");
+//Black
+Console.BackgroundColor = ConsoleColor.Black;
+Console.ForegroundColor = ConsoleColor.Black;
+Console.SetCursorPosition(3, 3); //Row 3
+Console.Write("   ");
+Console.SetCursorPosition(8, 3);
+Console.Write(" ");
+Console.SetCursorPosition(2, 4); //Row 4
+Console.Write(" ");
+Console.SetCursorPosition(4, 4);
+Console.Write(" ");
+Console.SetCursorPosition(8, 4);
+Console.Write(" ");
+Console.SetCursorPosition(2, 5); //Row 5
+Console.Write(" ");
+Console.SetCursorPosition(4, 5);
+Console.Write("  ");
+Console.SetCursorPosition(9, 5);
+Console.Write(" ");
+Console.SetCursorPosition(2, 6); //Row 6
+Console.Write("  ");
+Console.SetCursorPosition(8, 6);
+Console.Write("    ");
+//Magenta
+Console.BackgroundColor = ConsoleColor.Magenta;
+Console.ForegroundColor = ConsoleColor.Magenta;
+Console.SetCursorPosition(6, 3); //Row 3
+Console.Write("  ");
+Console.SetCursorPosition(9, 3);
+Console.Write(" ");
+Console.SetCursorPosition(3, 4); //Row 4
+Console.Write(" ");
+Console.SetCursorPosition(5, 4);
+Console.Write("   ");
+Console.SetCursorPosition(9, 4);
+Console.Write("   ");
+Console.SetCursorPosition(3, 5); //Row 5
+Console.Write(" ");
+Console.SetCursorPosition(6, 5);
+Console.Write("   ");
+Console.SetCursorPosition(10, 5);
+Console.Write("   ");
+Console.SetCursorPosition(4, 6); //Row 6
+Console.Write("    ");
+Console.SetCursorPosition(4, 7); //Row 7
+Console.Write("       ");
+Console.SetCursorPosition(1, 11); //Row 11
+Console.Write("  ");
+Console.SetCursorPosition(11, 11);
+Console.Write("  ");
+Console.SetCursorPosition(1, 12); //Row 12
+Console.Write("   ");
+Console.SetCursorPosition(10, 12);
+Console.Write("   ");
+Console.SetCursorPosition(1, 13); //Row 13
+Console.Write("  ");
+Console.SetCursorPosition(11, 13);
+Console.Write("  ");
+//Blue
+Console.BackgroundColor = ConsoleColor.Blue;
+Console.ForegroundColor = ConsoleColor.Blue;
+Console.SetCursorPosition(3, 8); //Row 8
+Console.Write("  ");
+Console.SetCursorPosition(6, 8);
+Console.Write("   ");
+Console.SetCursorPosition(2, 9); //Row 9
+Console.Write("   ");
+Console.SetCursorPosition(6, 9);
+Console.Write("  ");
+Console.SetCursorPosition(9, 9);
+Console.Write("   ");
+Console.SetCursorPosition(1, 10); //Row 10
+Console.Write("    ");
+Console.SetCursorPosition(6, 10);
+Console.Write("  ");
+Console.SetCursorPosition(9, 10);
+Console.Write("    ");
+Console.SetCursorPosition(3, 11); //Row 11
+Console.Write("  ");
+Console.SetCursorPosition(9, 11);
+Console.Write("  ");
+//Dark Yellow
+Console.BackgroundColor = ConsoleColor.DarkYellow;
+Console.ForegroundColor = ConsoleColor.DarkYellow;
+Console.SetCursorPosition(5, 12); //Row 12
+Console.Write(" ");
+Console.SetCursorPosition(8, 12);
+Console.Write(" ");
+//Gray, there isn't brown
+Console.BackgroundColor = ConsoleColor.DarkGray;
+Console.ForegroundColor = ConsoleColor.DarkGray;
+Console.SetCursorPosition(2, 15); //Row 15
+Console.Write("   ");
+Console.SetCursorPosition(8, 15);
+Console.Write("   ");
+Console.SetCursorPosition(1, 16); //Row 16
+Console.Write("    ");
+Console.SetCursorPosition(8, 16);
+Console.Write("    ");
+Console.SetCursorPosition(13, 17); //Moves the cursor so the bottom row of cyan won't be covered by terminal text
